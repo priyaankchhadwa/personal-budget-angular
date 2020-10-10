c0d3cc7e46ebf4bb45bfdaad5760534f24f92e46
priyaankchhadwa <priyaank29@gmail.com>
2020-10-09 22:50:30 -0400

    working code

--- personal-budget/src/app/app.module.ts
+++ personal-budget/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,9 @@ import { HeroComponent } from './hero/hero.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ArticleComponent } from './article/article.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { P404Component } from './p404/p404.component';
 
 @NgModule({
   declarations: [
@@ -16,10 +21,14 @@ import { ArticleComponent } from './article/article.component';
     HeroComponent,
     FooterComponent,
     HomepageComponent,
-    ArticleComponent
+    ArticleComponent,
+    AboutComponent,
+    LoginComponent,
+    P404Component
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- personal-budget/src/app/menu/menu.component.ts
+++ personal-budget/src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'pb-menu',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  public colors = [
+    '#0074D9',
+    '#FF4136',
+    '#2ECC40',
+    '#FF851B',
+    '#7FDBFF',
+    '#B10DC9',
+    '#FFDC00',
+    '#39CCCC',
+    '#39CCCC',
+    '#01FF70',
+    '#85144b',
+    '#F012BE',
+    '#3D9970',
+    '#111111',
+    '#AAAAAA',
+  ]
+
+  public dataSource = {
+    datasets: [
+      {
+        data: [],
+        backgroundColor: this.colors
+      }
+    ],
+    labels: []
+  };
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:3000/budget')
+      .subscribe((res: any) => {
+        for (let i = 0; i < res.myBudget.length; i++) {
+          this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+          this.dataSource.labels[i] = res.myBudget[i].title;
+          this.createChart();
+        }
+      });
+  }
+
+  // tslint:disable-next-line: typedef
+  public createChart() {
+    const ctx = document.getElementById('myChart');
+    const myPieChart = new Chart(ctx, {
+      type: 'pie',
+      data: this.dataSource
+    });
   }
 
 }
